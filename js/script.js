72087358f9073dfba59b5f5ecf37f4cbd4e3b911
bryangregory7 <140085099+bryangregory7@users.noreply.github.com>
2026-03-08 00:07:25 +0100

    Seperate items to different pages

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/* HEART MESSAGE */
+
+function toggleMessage(id){
+    let msg = document.getElementById(id);
+    msg.style.display = msg.style.display === "block" ? "none" : "block";
+}
+
+/* NIGHT MODE */
+
+function toggleNight(){
+    document.body.classList.toggle("night");
+}
+
+/* FLOATING HEARTS */
+
+setInterval(createHeart,800);
+
+function createHeart(){
+    const heart=document.createElement("div");
+    heart.className="floating-heart";
+    heart.innerHTML="❤️";
+    heart.style.left=Math.random()*100+"vw";
+    heart.style.fontSize=(20+Math.random()*20)+"px";
+    document.body.appendChild(heart);
+
+    setTimeout(()=>{heart.remove();},8000);
+}
+
+/* PASSWORD */
+
+function checkPassword(){
+    let password=document.getElementById("pw").value;
+
+    if(password==="iloveyou"){
+        document.getElementById("secretMessage").style.display="block";
+    }else{
+        alert("Wrong password ❤️");
+    }
+}
